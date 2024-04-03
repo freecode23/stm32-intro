4a21aceb8f64f4c73ae74ac4fe1ab0b7cba4ecd3
freecode23 <hartono.s@northeastern.edu>
2024-04-03 15:38:05 -0400

    Add LTE module with topic error

--- car_SIM7600/Core/Src/main.c
+++ car_SIM7600/Core/Src/main.c
@@ -70,7 +70,7 @@ const char password[] = "";
 const char topic_cmd[] = "topic/cmd";
 const char topic_sensor[] = "topic/sensor";
 const uint32_t timeOut = 10000;
-uint8_t rxBuffer[160] = {};
+uint8_t rxBuffer[160] = { };
 // Buffer to store the response
 uint8_t ATisOK = 0;
 uint8_t CGREGisOK = 0;
@@ -78,35 +78,86 @@ uint32_t previousTick;
 uint16_t readValue;
 char charData[15];
 
-
+/**
+ * Send an AT command to SIM module, and log the command and response to UART.
+ */
 void SIMTransmit(char *cmd) {
 
-	// Clear the receive buffer
+	// Clear the response buffer.
 	memset(rxBuffer, 0, sizeof(rxBuffer));
 
-	// Send the AT command to SIM7600
+	// Send the AT command to SIM7600.
 	HAL_UART_Transmit(&huart2, (uint8_t*) cmd, strlen(cmd), HAL_MAX_DELAY);
 	HAL_UART_Receive(&huart2, (uint8_t*) rxBuffer, sizeof(rxBuffer), 5000);
 
+	// Log the response received by the SIM module.
 	char status_msg[200];
 	sprintf(status_msg, "-->Command and res:\n%s\r\n", rxBuffer);
 	HAL_UART_Transmit(&huart3, (uint8_t*) status_msg, strlen(status_msg),
 	HAL_MAX_DELAY);
 
+}
 
+uint8_t SIMTransmitWithPrompt(const char *cmd, const char *additionalData) {
+	// Clear the receive buffer
+	memset(rxBuffer, 0, sizeof(rxBuffer));
+	HAL_UART_Transmit(&huart2, (uint8_t*) cmd, strlen(cmd), HAL_MAX_DELAY);
+	uint32_t startTime = HAL_GetTick();
+
+	// Loop until prompt is received or timeout occurs
+	while (strstr((char*) rxBuffer, ">") == NULL) {
+		HAL_UART_Receive(&huart2, (uint8_t*) rxBuffer, sizeof(rxBuffer), 3000);
+
+		// Check timeout
+		if ((HAL_GetTick() - startTime) > 5000) {
+			char status_msg[200];
+			sprintf(status_msg, "-->Time out!\r\n");
+			HAL_UART_Transmit(&huart3, (uint8_t*) status_msg,
+					strlen(status_msg), HAL_MAX_DELAY);
+			break;
+		}
+	}
+
+	// Log the response from sending AT command.
+	char status_msg[200];
+	sprintf(status_msg, "-->Initial Response:\n%s\r\n", rxBuffer);
+	HAL_UART_Transmit(&huart3, (uint8_t*) status_msg, strlen(status_msg),
+			HAL_MAX_DELAY);
+
+	// Check if the response include the prompt ">"
+	if (strstr((char*) rxBuffer, ">") != NULL) {
+		HAL_UART_Transmit(&huart2, (uint8_t*) additionalData,
+				strlen(additionalData), HAL_MAX_DELAY);
+
+		// Clear the buffer again to receive the final response
+		memset(rxBuffer, 0, sizeof(rxBuffer));
+		HAL_UART_Receive(&huart2, (uint8_t*) rxBuffer, sizeof(rxBuffer), 5000);
+
+		// Log the final response.
+		char status_msg[200];
+		sprintf(status_msg, "-->Final response:\n%s\r\n", rxBuffer);
+		HAL_UART_Transmit(&huart3, (uint8_t*) status_msg, strlen(status_msg),
+				HAL_MAX_DELAY);
+		return 1;
+
+	} else {
+
+		sprintf(status_msg, "-->Prompt not found\r\n");
+		return 0;
+	}
 }
 
 void mqttPublish(void) {
 	ATisOK = 0;
 	CGREGisOK = 0;
-	char ATcommand[80];
+	char ATcommand[256];
+	char additionalData[256];
 
 	// 1. Check for OK response for AT
 	previousTick = HAL_GetTick();
 	while (!ATisOK && previousTick + timeOut > HAL_GetTick()) {
 		SIMTransmit("AT\r\n");
-		if (strstr((char*) rxBuffer, "OK"))
-				{
+		if (strstr((char*) rxBuffer, "OK")) {
 			ATisOK = 1;
 		}
 
@@ -114,15 +165,14 @@ void mqttPublish(void) {
 
 	}
 
-	// 2. Ensure GPRS network is available
+	// 2. Ensure GPRS network is available.
 	if (ATisOK) {
 		previousTick = HAL_GetTick();
 		while (!CGREGisOK && previousTick + timeOut > HAL_GetTick()) {
 			SIMTransmit("AT+CSQ\r\n");
-			SIMTransmit("AT+CREG?\r\n");
-			SIMTransmit("AT+CGREG?\r\n");
-			if (strstr((char*) rxBuffer, "+CGREG: 0,1"))
-					{
+			SIMTransmit("AT+CREG?\r\n"); // voice/data calls and SMSs
+			SIMTransmit("AT+CGREG?\r\n"); // network allowing the access to internet.
+			if (strstr((char*) rxBuffer, "+CGREG: 0,1")) {
 				CGREGisOK = 1;
 			}
 		}
@@ -131,7 +181,7 @@ void mqttPublish(void) {
 	// If registered
 	if (CGREGisOK) {
 		// 1.Ensure GPRS network is available.
-		sprintf(ATcommand, "AT+CGSOCKCONT=1,\"IP\",\"%s\"\r\n", apn); // Specify the value of  PDP context
+		sprintf(ATcommand, "AT+CGSOCKCONT=1,\"IP\",\"%s\"\r\n", apn);
 		SIMTransmit(ATcommand);
 		SIMTransmit("AT+CGPADDR\r\n");
 
@@ -145,17 +195,48 @@ void mqttPublish(void) {
 		sprintf(ATcommand, "AT+CMQTTCONNECT=0,\"%s:%d\",60,1\r\n", host, port);
 		SIMTransmit(ATcommand);
 
-		// 5. Set the topic for publish message "topic/cmd"
-		sprintf(ATcommand, "AT+CMQTTTOPIC=0,%d\r\n", strlen(topic_cmd));
-		SIMTransmit(ATcommand);
 
+		// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+		// 5. Set the topic for publish message "topic/cmd".
+		// Question1: How to send a multipart message?
+		// When we send the first command to set the topic, we are suppposed to receive a ">" as reposnse.
+		// Once we receive that response, we should follow up and send the name of the topic.
+		// What is the best way to do this ?
+		// I noticed that AT+CGREG gives me 0,3 reponse instead of 0,1. Could this be the reason?
+		// I have tried 2 options:
+		// Option 1: Use \r\n in both command and topic and don't bother to use while loop to check whether we have receivied the prompt signal ">".
+		// I see this working in a youtuber video.
+//		sprintf(ATcommand,"AT+CMQTTTOPIC=0,%d\r\n",strlen(topic_cmd));
+//		SIMTransmit(ATcommand);
 //		sprintf(ATcommand,"%s\r\n",topic_cmd);
 //		SIMTransmit(ATcommand);
 
-		// TODO: topic length received is 0,11 ERROR eventhough we haven't actually send the topic.
-		// Sending the topic string without adding \r\n to its length
+		// Option 2: Use while loop and wait for the ">" sign before sending the topic name.
+		// This also doesn't work. It always hits timeout.
+		sprintf(ATcommand,"AT+CMQTTTOPIC=0,%d",strlen(topic_cmd));
+		SIMTransmit(ATcommand);
+		sprintf(additionalData,"%s\r\n",topic_cmd);
+		SIMTransmitWithPrompt(ATcommand, additionalData);
+
+		// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+		// Question 2. Why do I need to always restart the SIM module in order to not have the followiing error:
+//		-->Command and res:
+//		AT+CGPADDR
 //
-//		// 6. Set the payload
+//		+CGPADDR: 1,100.111.74.74
+//		+CGPADDR: 2,0.0.0.0,0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0
+//		+CGPADDR: 3,0.0.0.0,0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0
+//		+CGPADDR: 4,0...
+//		-->Command and res:
+//		0AT+CMQTTSTART
+//
+//		+CMQTTSTART: 23
+//
+//		ERROR
+
+		// 6. Set the payload
 //		sprintf(ATcommand, "AT+CMQTTPAYLOAD=0,%d\r\n",
 //				strlen("Hello from sim7600"));
 //		SIMTransmit(ATcommand);
@@ -168,7 +249,6 @@ void mqttPublish(void) {
 //		SIMTransmit("AT+CMQTTDISC=0,120\r\n"); // Disconnect from Server
 //		SIMTransmit("AT+CMQTTREL=0\r\n"); // Release the Client
 //		SIMTransmit("AT+CMQTTSTOP\r\n"); // Stop MQTT Service
-
 
 	}
 }
